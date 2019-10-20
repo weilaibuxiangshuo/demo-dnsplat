7cb567e2d30e986fbdce5fce821080323fe86842
weilaibuxiangshuo <55013875+weilaibuxiangshuo@users.noreply.github.com>
2019-10-20 18:00:41 +0800

    Update dns1.lua

--- dns1.lua
+++ dns1.lua
@@ -43,33 +43,7 @@ for key,val in pairs(aa) do
 table.insert(record_total,key)
 end
 sum_total={
-{"kehu30.npjq3g.com","http://www.18ab.cc"},
-{"kehu37.uomko9.com","https://www.071515.com"},
-{"halin877.nx7aun.com","https://www.amjsty.com"},
-{"20190321.g4v94e.com","https://www.01338.com"},
-{"3415459394.xbpdj2.com","http://yyyy.dnstiao.com"},
-{"xhh899.ay3un5.com","http://ymty.dns66666.com"},
-{"kkk.ppvnv56.me","http://ymty.dns66666.com"},
-{"kehu32.mvgf8n.com","http://www.2004655.com"},
-{"3014002271.f8rdgu.com","http://www.www848585.com"},
-{"1426955253.nx7aun.com","http://www.c67783.com"},
-{"1426955253.f8rdgu.com","http://www.ziuxcyhwuqyhrfiwhsnf.com"},
-{"1426955253.qehl2q.com","http://www.c67783.com"},
-{"3014002271.qehl2q.com","http://www.51gongbao.com/app/gongbao.html"},
-{"3270065774.ryu6q.com","http://www.4591jc.com"},
-{"3557886598001.pye3q.com","http://www.1786900.com"},
-{"3557886598002.pye3q.com","http://www.980868.com"},
-{"3557886598003.pye3q.com","http://www.1008929.com"},
-{"qqcom.tip7g.com","http://ymty.dns66666.com"},
-{"2017032701.uomko9.com","http://www.1366891.com"},
-{"2028564679.g4v94e.com","https://www.1909955.com"},
-{"tencent.xhh327.com","https://www.meishij.net"},
-{"3464729339.xbpdj2.com","http://www.58166.com"},
-{"3464729339.ryu6q.com","http://103.233.9.26"},
-{"3270065774.ncu8j.com","http://www.1123zz.com"},
-{"1783830454.ncu8j.com","http://daka3d.com/yidong/dake.html"},
-{"3014002271.u3khfd.com","http://www.jurong168.com/app/suzhou.html"},
-{"hai.pp2cc7j.me","http://www.163.com"},
+{"ccc.uuu.com","http://www.baidu.com"},
 }
 for key,val in pairs(sum_total) do
     for kk,jj in pairs(record_total) do
