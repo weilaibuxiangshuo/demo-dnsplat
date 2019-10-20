48ff57d3a82c635b0374780095dcd38c2db52040
weilaibuxiangshuo <55013875+weilaibuxiangshuo@users.noreply.github.com>
2019-10-20 17:59:42 +0800

    Update views.py

--- plat/views.py
+++ plat/views.py
@@ -27,31 +27,7 @@ class Md1(MiddlewareMixin):
 
 def shuju(url):
     url_dict = {
-        "kehu30.npjq3g.com": "http://www.18ab.cc",
-        "kehu37.uomko9.com": "http://www.795969.com",
-        "halin877.nx7aun.com": "http://www.amjsty.com",
-        "20190321.g4v94e.com": "https://www.01338.com",
-        "3415459394.xbpdj2.com": "http://yyyy.dnstiao.com",
-        "xhh899.ay3un5.com": "http://ymty.dns66666.com",
-        "kkk.ppvnv56.me": "http://ymty.dns66666.com",
-        "kehu32.mvgf8n.com": "http://www.2004655.com",
-        "3014002271.f8rdgu.com": "http://www.www876262.com",
-        "1426955253.nx7aun.com": "http://www.c67783.com",
-        "1426955253.f8rdgu.com": "http://www.ziuxcyhwuqyhrfiwhsnf.com",
-        "1426955253.qehl2q.com": "http://www.c67783.com",
-        "3014002271.qehl2q.com": "http://www.51gongbao.com/app/gongbao.html",
-        "3270065774.ryu6q.com": "http://www.4591jc.com",
-        "3557886598001.pye3q.com": "http://www.1786900.com",
-        "3557886598002.pye3q.com": "http://www.980868.com",
-        "3557886598003.pye3q.com": "http://www.1008929.com",
-        "qqcom.tip7g.com":"http://ymty.dns66666.com",
-        "2017032701.uomko9.com": "http://www.1366891.com",
-        "3014002271.g4v94e.com": "http://www.jurong168.com/app/suzhou.html",
-        "tencent.xhh327.com": "https://www.meishij.net",
-        "3464729339.xbpdj2.com": "http://www.58166.com",
-        "3464729339.ryu6q.com": "http://103.233.9.26",
-        "3270065774.ncu8j.com": "http://www.1123zz.com",
-        "1783830454.ncu8j.com": "http://daka3d.com/yidong/dake.html",
+        "ccc.uuu.com": "http://www.baidu.com",
     }
     cc_list = []
     for cc, aa in url_dict.items():
